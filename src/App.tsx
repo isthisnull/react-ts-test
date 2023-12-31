7815e346f3de3683d190d074d8b01b6d604d3d07
isthisnull <letcodewithzanjiri@hotmail.com>
2023-12-31 23:31:48 +0330

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import React from "react";
 import Heading from "./Components/Heading.tsx";
-
+import { useState } from "react";
+import Counter from "./Components/Counter.tsx";
 function App() {
-  return <></>;
+  const [count, setCount] = useState<number>(1);
+  return (
+    <>
+      <Heading title="kirekhar" />
+      <Counter setCount={setCount}>Count is {count}</Counter>
+    </>
+  );
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import viteTsconfigPaths from "vite-tsconfig-paths";
-import svgrPlugin from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +7,5 @@ export default defineConfig({
     react({
       jsxRuntime: "classic", // Add this line
     }),
-    viteTsconfigPaths(),
-    svgrPlugin(),
   ],
 });
