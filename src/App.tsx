e7cc0011bfa3ac1b063134568bba99409f4866f7
isthisnull <letcodewithzanjiri@hotmail.com>
2024-01-02 17:08:01 +0330

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { Children } from "react";
 import Heading from "./Components/Heading.tsx";
 import { useState } from "react";
 import Counter from "./Components/Counter.tsx";
+import Popup from "./Components/Popup.tsx";
+import Stringer from "./Components/Stringer.tsx";
 function App() {
   const [count, setCount] = useState<number>(1);
+  const [koni, setKoni] = useState<string>("");
   return (
     <>
       <Heading title="kirekhar" />
       <Counter setCount={setCount}>Count is {count}</Counter>
+      <Popup message="kiriii"></Popup>
+      <Stringer setKoni={setKoni}>man chiam? {koni}</Stringer>
     </>
   );
 }
---
+++ src/Components/Stringer.tsx
@@ -0,0 +1,30 @@
+import React, { ReactNode } from "react";
+
+interface StringerProps {
+  setKoni: React.Dispatch<React.SetStateAction<string>>;
+  children?: ReactNode;
+}
+
+const Stringer = ({ setKoni, children }: StringerProps) => {
+  return (
+    <>
+      <button
+        onClick={() => {
+          setKoni("Kiri Hastam");
+        }}
+      >
+        kiri hasti
+      </button>
+      <button
+        onClick={() => {
+          setKoni("Koni Hastam");
+        }}
+      >
+        koni hasti
+      </button>
+      <h4>{children}</h4>
+    </>
+  );
+};
+
+export default Stringer;
